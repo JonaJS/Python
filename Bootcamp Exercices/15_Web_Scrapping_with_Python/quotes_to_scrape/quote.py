09f5638a3df4af8f1cd9ba778b14c382306b16e3
Jonathan <jjimnezs@gmail.com>
2021-10-24 00:19:45 -0700

    Quotes Scrapper code finished

--- Bootcamp Exercices/15_Web_Scrapping_with_Python/quotes_to_scrape/quote.py
+++ Bootcamp Exercices/15_Web_Scrapping_with_Python/quotes_to_scrape/quote.py
@@ -19,14 +19,42 @@ class Quote:
         enhaced_authors_list = [author.text for author in raw_authors_list]
         return enhaced_authors_list
 
+
     def get_main_page_quotes(self):
         soup = self.get_main_page_scrapped()
         raw_quotes_list = soup.select(".text")
         enhaced_quotes_list = [quote.text for quote in raw_quotes_list]
         return enhaced_quotes_list
 
+
     def get_top_ten_tags(self):
         soup = self.get_main_page_scrapped()
         tag_box = soup.select("span > a")
         top_ten_tag_list = [tag.text for tag in tag_box if tag.text != "(about)"]
         return top_ten_tag_list
+
+    
+    def get_all_authors(self):
+        page_number = 1
+        author_list = []
+        
+        while True:
+            new_url = f"{self.url}page/{page_number}/"
+
+            res = requests.get(new_url)
+            soup = bs4.BeautifulSoup(res.text, 'lxml')
+            raw_authors_list = soup.select(".author")
+        
+            for author in raw_authors_list:
+                if author.text not in author_list:
+                    author_list.append(author.text)
+
+            if soup.select(".next"):
+                page_number += 1
+            else:
+                break
+
+        return author_list
+
+
+
